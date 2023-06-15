75b70975e54d3731164df9369ee82c06c1ed84a4
medxpy <m3dh4ck@protonmail.ch>
2023-06-15 16:48:57 -0700

    5-Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Description: printf
+ * Return: Always 0 (Success)
+ */
+
+int	main(void)
+{
+	char string[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s", string);
+	return (0);
+}
