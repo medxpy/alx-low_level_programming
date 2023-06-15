4f3e1e28a25e30ebb3cdc93c20eaf2bf7c89a603
medxpy <m3dh4ck@protonmail.ch>
2023-06-15 10:22:03 -0700

    4-Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,16 @@
 #include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Printing a whole string with a double quatation mark using puts() function
+ *
+ * Return: Always 0 (Success)
+ */
 
 int	main(void)
 {
 	char string[] = "\"Programming is like building a multilingual puzzle";
-	
+
 	puts(string);
 	return (0);
 }
