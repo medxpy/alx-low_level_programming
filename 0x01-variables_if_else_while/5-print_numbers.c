e60d5945d4b3f270164fe13e577feeca293e1c7e
medxpy <m3dh4ck@protonmail.ch>
2023-06-16 15:15:29 -0700

    5-numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include<sdtio.h>
+
+/**
+ * main - Entry point
+ * Description: Print all single digit numbers strating from 0
+ * Return: always 0 on success
+ */
+
+int	main(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		printf("%d\n", i);
+		i++;
+	}
+	return (0);
+}
