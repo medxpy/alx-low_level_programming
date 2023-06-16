8473b6c95c0d300e9f700d598a2612fb8c7bbb56
medxpy <m3dh4ck@protonmail.ch>
2023-06-16 15:05:29 -0700

    4-When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Print alphabet exept q and e 
+ * Return: Alwyas 0 on success
+ */
+
+int	main(void)
+{
+	char i;
+
+	i = 97;
+	while (i <= 122)
+	{
+		if ((i != 101) && (i != 113))
+		{
+			putchar(i);
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
+
