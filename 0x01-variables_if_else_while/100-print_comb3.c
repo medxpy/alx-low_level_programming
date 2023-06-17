f8a31d12dd6a5577e2240de781aa3840fc5faa58
medxpy <m3dh4ck@protonmail.ch>
2023-06-17 13:44:19 -0700

    10-Print all possible differents combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Description: Prints all possible differents combinations
+ * of two digits.
+ * Return: Always 0 on success
+ * By Medxpy on 17/jun/2023 21:33
+ */
+
+int	main(void)
+{
+	int i;
+	int j;
+
+	i = 0;
+
+	while (i <= 9)
+	{
+		j = i + 1;
+		while (j <= 9)
+		{
+			putchar('0' + i);
+			putchar('0' + j);
+			if ((i != 8) && (j != 9))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
