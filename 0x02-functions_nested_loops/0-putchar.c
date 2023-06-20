ce30bc0c2253cde97cba64d8e4cfd787fac65b40
medxpy <m3dh4ck@protonmail.ch>
2023-06-20 12:17:09 -0700

    starting 0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: Print _putchar followed by new line
+ * Return: Always 0 on success
+ * By Medxpy
+ */
+
+int	main(void)
+{
+	write(1, "_putchar", 8);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,10 @@
+# 0x02. C - Functions, nested loops
+
+## Tasks
+
+### 0- _putchar
+
+Write a program that prints _putchar, followed by a new line.
+
+ - The program should return `0`
+
