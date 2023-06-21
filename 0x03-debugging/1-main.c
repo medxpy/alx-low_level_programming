f4caac032188156abc74552fe72a8c4fc77b8072
medxpy <m3dh4ck@protonmail.ch>
2023-06-21 07:44:54 -0700

    1-Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,13 +12,12 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-	/**
-	 * the while loop didn't get incremented
-	 */
-	while (i < 10)
-	{
-		putchar(i);
-	}
+/*
+ *	while (i < 10)
+ *	{
+ *		 putchar(i);
+ *	}
+ */
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
