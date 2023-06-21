849e42057788d30377625d2b5bdb824d9db1caa5
medxpy <m3dh4ck@protonmail.ch>
2023-06-20 19:29:46 -0700

    10- a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add() - adds two integers and return result
+ * @a: first integer
+ * @b: second inetger
+ * Return: result of add a + b
+ */
+
+int	add(int a, int b)
+{
+	return (a + b);
+}
+
