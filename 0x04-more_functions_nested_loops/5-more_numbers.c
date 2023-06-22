ea3079de72c214d3d0f0eaabe1d6f3d831defcb6
medxpy <m3dh4ck@protonmail.ch>
2023-06-22 14:56:01 -0700

    5- more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 10 times the numbers, from
+ * 0 to 14, followed by a new line
+ * Rerurn: Always 0 on success
+ */
+
+void	more_numbers(void)
+{
+	int number;
+	int i;
+
+	number = 0;
+	i = 0;
+	while (i <= 10)
+	{
+		while (number <= 14)
+		{
+			if (number >= 10)
+				_putchar('0' + number / 10);
+			_putchar('0' + number % 10);
+			number++;
+		}
+		_putchar('\n');
+		i++;
+		number = 0;
+	}
+}
