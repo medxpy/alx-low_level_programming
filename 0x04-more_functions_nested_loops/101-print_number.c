0b54db65549c33a93ec7f6769f48c8f0d5443bff
medxpy <m3dh4ck@protonmail.ch>
2023-06-23 14:27:19 -0700

    final fix commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,19 +7,17 @@
 
 void	print_number(int n)
 {
-	unsigned number = n;
+	unsigned int number = n;
 
 	if (n < 0)
 	{
-		number = -n
 		_putchar('-');
+		number = -number;
 	}
-	else if (number > 9)
+	if (number > 9)
 	{
 		print_number(number / 10);
-		print_number(number % 10);
 	}
-	else
-		_putchar(number + '0');
+	_putchar(number % 10 + '0');
 }
 
