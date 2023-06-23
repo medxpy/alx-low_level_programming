f7db06754a14d85b4f3f1db0b4d9f717e502c3d0
medxpy <m3dh4ck@protonmail.ch>
2023-06-23 14:05:40 -0700

    fexing missed ;

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,17 +7,19 @@
 
 void	print_number(int n)
 {
+	unsigned number = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		print_number(-n);
+		print_number(-number);
 	}
-	else if (n > 9)
+	else if (number > 9)
 	{
-		print_number(n / 10);
-		print_number(n % 10);
+		print_number(number / 10);
+		print_number(number % 10);
 	}
 	else
-		_putchar(n + '0');
+		_putchar(number + '0');
 }
 
