c9fa376e1cd93cf305b0a99dbf6ebf36010e728b
medxpy <m3dh4ck@protonmail.ch>
2023-06-26 13:18:07 -0700

    5- reverse a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,25 +1,6 @@
 #include "main.h"
 
 /**
- * rev_string - reverse a string
- * @s: The string to reverse
- * Return: Anything
- * By Medxpy
- */
-
-void	rev_string(char *s)
-{
-	int i;
-
-	i = _strlen(s) - 1;
-	while (s[i] != _strlen(s))
-	{
-		_putchar(s[i]);
-		i--;
-	}
-}
-
-/**
  * _strlen - calculate the length of string
  * @s: the string to calculate length to
  * Return: the length
@@ -34,3 +15,24 @@ int	_strlen(char *s)
 		i++;
 	return (i);
 }
+
+
+/**
+ * rev_string - reverse a string
+ * @s: The string to reverse
+ * Return: Anything
+ * By Medxpy
+ */
+
+void	rev_string(char *s)
+{
+	int i;
+
+	i = _strlen(s) - 1;
+	while (s <= _strlen(s))
+	{
+		_putchar(s[i]);
+		i--;
+	}
+}
+
