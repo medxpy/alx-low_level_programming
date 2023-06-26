03824cca6d8a09cb8a9c38bf5053c0ebeff4a3fa
medxpy <m3dh4ck@protonmail.ch>
2023-06-26 07:21:43 -0700

    0- 98 Bettery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update the value it points to to 98
+ * @n: the parameter to update its value
+ * Return: nothing
+ * By Medxpy
+ */
+
+void	reset_to_98(int *n)
+{
+	*n = 98;
+}
