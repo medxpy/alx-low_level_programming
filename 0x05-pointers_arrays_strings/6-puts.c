23dfd024268a0f5c426801cea20ee4ab5666ae0f
medxpy <m3dh4ck@protonmail.ch>
2023-06-26 13:56:19 -0700

    6. Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strlen - calculate the length of a string
+ * @str: the string to calculate length to
+ * Return: The length;
+ */
+
+int	_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (i != '\0')
+		i++;
+	return (i);
+}
+
+/**
+ * puts2 - print every other character of a string
+ * @str: the string to print other character
+ * Return: Anything
+ */
+
+void	puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (i < _strlen(str))
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar('\n');
+}
