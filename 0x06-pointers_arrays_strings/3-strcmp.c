10caa772895f0ccd5a3247afbd73236ea5adbf60
medxpy <m3dh4ck@protonmail.ch>
2023-06-29 16:52:59 -0700

    3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,12 +12,10 @@ int	_strcmp(char *s1, char *s2)
 	int i;
 
 	i = 0;
-	while (s1[i] || s2[i])
+	while (s1[i] != '\0' || s2[i] != '\0')
 	{
-		if (s1[i] > s2[i])
-			return (1);
-		else if (s1[i] < s2[i])
-			return (-1);
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 		i++;
 	}
 	return (0);
