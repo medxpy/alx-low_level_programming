aa9d407e6200358f2237cc5910019bc4eb94f09a
medxpy <m3dh4ck@protonmail.ch>
2023-07-07 19:41:53 -0700

    print name of the program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print out the name of the program
+ * @argc: number of arg
+ * @argv: array of arg
+ * Return: always 0
+ */
+
+int	main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", *argv);
+	return (0);
+}
+
