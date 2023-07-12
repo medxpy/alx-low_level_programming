45e5ed0954a86b7f3ffe0029105ec3078f62a1a7
medxpy <m3dh4ck@protonmail.ch>
2023-07-12 06:54:29 -0700

    1-strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *_strdup - Duplicate a string
+ * @str: The string to duplicate
+ * Return: Pointer to the duplicated string, NULL if insufficient memory
+ */
+
+char	*_strdup(char *str)
+{
+	char* newStr;
+	unsigned int i;
+	int len;
+
+	if (str == NULL)
+		return (NULL);
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	newStr = (char*)malloc((len + 1) * sizeof(char));
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		newStr[i] = str[i];
+		i++;
+	}
+	newStr[i] = '\0';
+	return (newStr);
+}
+
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -9,3 +9,7 @@ Write a function that creates an array of chars, and initializes it with a speci
  - Returns `NULL` if size = `0`
  - Returns a pointer to the array, or `NULL` if it fails
 
+### 1. The woman who has no imagination has no wings
+Write a function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.
+
+
